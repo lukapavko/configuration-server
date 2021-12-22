9af1ecfd4460b2316738138f577c4432db310f21
Luka <luka.pavkovic@net4market.hr>
2021-12-22 12:37:23 +0100

    Initial commit

---
+++ src/test/java/com/logbook/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logbook.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
